05f963c94108470e477aeaa835748d90516275d1
horhik <horhik@tuta.io>
2021-08-31 23:45:19 +0300

    clean up

--- default.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs ? import <nixpkgs> {} }:
-
-pkgs.python3Packages.buildPythonApplication {
-  pname = "innubis";
-  src = ./.;
-  version = "0.1";
-  propagatedBuildInputs = [ pkgs.python310Packages.pip ];
-}
