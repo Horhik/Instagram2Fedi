971e74d254c52b881852afab502131cdad9f4fc3
horhik <horhik@tuta.io>
2021-11-22 15:41:47 +0300

    add more variabes in env.sh

--- env.sh
+++ env.sh
@@ -1,8 +1,13 @@
 #!b/in/sh
 
+# Required
 export YOUR_CONTAINER_NAME=
 export I2M_INSTAGRAM_USER=
 export I2M_INSTANCE=
 export I2M_TOKEN=
 
+export I2M_CHECK_INTERVAL=3600 #1 hour
+export I2M_POST_INTERVAL=3600 #1 hour
+export I2M_USE_MASTODON=4 #max carousel length is 4, if there's no limit set to -1
+export I2M_FETCH_COUNT=5 # how many instagram posts to fetch per check_interval
 
